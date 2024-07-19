539f0bc9a1c7da5f001dfc212ea5e0d622a4e199
ElsenV <elsen.velibeyli@mail.ru>
2024-07-19 09:19:29 +0400

    export lineside enum

--- lib/app_tutorial.dart
+++ lib/app_tutorial.dart
@@ -3,3 +3,4 @@ library tutorial;
 export 'src/models/tutorial_item.dart';
 export 'src/controller/tutorial_tutorial.dart';
 export 'src/models/shape_model_enum.dart';
+export 'src/models/line_side_enum.dart';
